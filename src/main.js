10973ab5116fe27a31bafe140caed398095e561e
Oluwwatimilehin Awoniyi <awoniyitimi@outlook.com>
2022-08-21 14:54:19 +0100

    improved on functionality

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 const profile = document.querySelector(".profile-pic");
 const profileInfo = document.querySelector(".profile__dropdown");
 const arrow = document.querySelector(".arrow");
+const dots = document.querySelectorAll(".btn-dots");
 
 function show(app, arr) {
   app.classList.toggle("hide");
   arr.classList.toggle("bx-chevron-up");
 }
+
+function openInfo() {
+  closeInfo(this);
+  this.parentElement.parentElement.lastElementChild.classList.toggle("hide");
+}
+function closeInfo(infoOne) {
+  dots.forEach((infoTwo) => {
+    if (infoTwo != infoOne) {
+      infoTwo.parentElement.parentElement.lastElementChild.classList.add(
+        "hide"
+      );
+    }
+  });
+}
+
+dots.forEach((i) => i.addEventListener("click", openInfo));
