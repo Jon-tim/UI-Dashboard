574e3e354deddcf851436c7e7a28170a7be9480f
Oluwwatimilehin Awoniyi <awoniyitimi@outlook.com>
2022-08-21 16:56:20 +0100

    updated navbar

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ const profile = document.querySelector(".profile-pic");
 const profileInfo = document.querySelector(".profile__dropdown");
 const arrow = document.querySelector(".arrow");
 const dots = document.querySelectorAll(".btn-dots");
+const navIcons = document.querySelectorAll(".nav-item");
+const navToggler = document.querySelector(".nav-toggler");
+const nav = document.querySelector("nav");
+const navItemName = document.querySelectorAll(".nav-item span");
+const settingsName = document.querySelectorAll(".settings span");
 
 function show(app, arr) {
   app.classList.toggle("hide");
@@ -23,3 +28,25 @@ function closeInfo(infoOne) {
 }
 
 dots.forEach((i) => i.addEventListener("click", openInfo));
+
+function activeNavIcon() {
+  nonActive(this);
+  this.classList.add("active");
+}
+
+function nonActive(arg) {
+  navIcons.forEach(function (i) {
+    if (arg != i) {
+      i.classList.remove("active");
+    }
+  });
+}
+
+navIcons.forEach((i) => i.addEventListener("click", activeNavIcon));
+
+function openNav() {
+  settingsName.forEach((i) => i.classList.toggle("hide__navlist__names"));
+  navItemName.forEach((i) => i.classList.toggle("hide__navlist__names"));
+  nav.classList.toggle("nav-open");
+}
+navToggler.addEventListener("click", openNav);
